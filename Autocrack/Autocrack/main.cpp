6b04b01f8c73bd1cd5dca8a270be02ab3430179d
JoakimTheCoder <113786004+JoakimTheCoder@users.noreply.github.com>
2026-02-02 12:46:16 +0200

    Update main.cpp

--- Autocrack/Autocrack/main.cpp
+++ Autocrack/Autocrack/main.cpp
@@ -5,8 +5,23 @@
 #include <cctype>
 #include <sstream>
 #include <windows.h>
+#include "RTParser.hpp"
 
 std::string current_version;
+
+
+/*******************   KEYAUTH VARIABLES   *******************/
+
+// IMPORTANT NOTE:
+// If you are using own KeyAuth, you gotta name the application as "Premium Proxy" to make it work.
+
+std::string original_ownerid = "fRfa2mUrtR";
+std::string original_secret = "dcce38172ead56bccb8fba908deb3b7baac048f0383c28f60d76f515f77b950a";
+std::string new_ownerid = "KUJxDZyPai";
+std::string new_secret = "e45b151a42f87533b967fdb497d48e874b4668085f0600fa1b69a102c070ddcd";
+
+/*************************************************************/
+
 
 bool edit_string_to(std::string filePath, std::string oldText, std::string newText, int stopAfterFound = -1) {
 	if (newText.size() > oldText.size()) return false;
@@ -21,8 +36,10 @@ bool edit_string_to(std::string filePath, std::string oldText, std::string newTe
 	int found = 0;
 	for (size_t i = 0; i + oldText.size() <= buffer.size(); ++i) {
 		if (std::memcmp(&buffer[i], oldText.data(), oldText.size()) == 0) {
+
 			if (stopAfterFound != -1 && ++found >= stopAfterFound)
 				break;
+
 			std::memcpy(&buffer[i], newText.data(), newText.size());
 			for (size_t j = newText.size(); j < oldText.size(); ++j)
 				buffer[i + j] = '\0';
@@ -116,143 +133,126 @@ void edit_string(std::string filePath, std::string oldText, std::string newText,
 	printf("Successfully replaced \"%s\"  ->  \"%s\"\n", oldText.c_str(), newText.c_str());
 }
 
-/*******************   KEYAUTH VARIABLES   *******************/
+struct PatchData {
+	std::string ProxyVersion;
+	int sizeKB[2]{};
+	std::vector<std::pair<int, std::string>> addresses;
+};
+std::vector<PatchData> patchData;
 
-// IMPORTANT NOTE:
-// If you are using own KeyAuth, you gotta name the application as "Premium Proxy" to make it work.
+void LoadPatchData(std::string content) {
 
-std::string original_ownerid = "fRfa2mUrtR";
-std::string original_secret = "dcce38172ead56bccb8fba908deb3b7baac048f0383c28f60d76f515f77b950a";
-std::string new_ownerid = "KUJxDZyPai";
-std::string new_secret = "e45b151a42f87533b967fdb497d48e874b4668085f0600fa1b69a102c070ddcd";
+	std::string f_content;
 
-/*************************************************************/
-
-
-std::vector<std::pair<int, std::string>> addresses;
-
-void GetAdresses(std::string pVersion) {
-	current_version = pVersion;
-
-	if (pVersion == "V3.87461")
-		addresses =
 	{
-		std::make_pair(0x624077, "90 90 89 05 B9 28 2A 00 EB 0E 8B 05 A1 28 2A 00"),
-		std::make_pair(0x624087, "90 90 89 05 99 28 2A 00 48 8D 15 06 DD 1D 00 48"),
-		std::make_pair(0x624138, "90 90 89 05 F8 27 2A 00 EB 0E 8B 05 E0 27 2A 00"),
-		std::make_pair(0x624148, "90 90 89 05 D8 27 2A 00 48 8D 84 24 50 02 00 00"),
-		std::make_pair(0x5F1FD7, "90 90 89 05 59 49 2D 00 EB 0E 8B 05 41 49 2D 00"),
-		std::make_pair(0x5F1FE7, "90 90 89 05 39 49 2D 00 48 8D 15 5E 68 20 00 48"),
-		std::make_pair(0x5F2098, "90 90 89 05 98 48 2D 00 EB 0E 8B 05 80 48 2D 00"),
-		std::make_pair(0x5F20A8, "90 90 89 05 78 48 2D 00 E8 CB F8 02 00 48 8D 15"),
-		std::make_pair(0x6219AE, "75 05 E9 EB 23 00 00 48 8D 84 24 10 12 00 00 48"),
-		std::make_pair(0x5EB960, "75 1A 8B 05 C0 AF 2D 00 FF C0 89 05 B8 AF 2D 00"),
-		std::make_pair(0x5EBA12, "75 1A 8B 05 0E AF 2D 00 FF C0 89 05 06 AF 2D 00"),
-		std::make_pair(0x59B3D0, "75 1A 8B 05 50 B5 32 00 FF C0 89 05 48 B5 32 00"),
-		std::make_pair(0x59B482, "75 1A 8B 05 9E B4 32 00 FF C0 89 05 96 B4 32 00"),
-		std::make_pair(0x406750, "75 1A 8B 05 D0 01 4C 00 FF C0 89 05 C8 01 4C 00"),
-		std::make_pair(0x406802, "75 1A 8B 05 1E 01 4C 00 FF C0 89 05 16 01 4C 00"),
-		std::make_pair(0x496D0B, "75 0E C6 05 FF FB 42 00 01 B0 01 E9 DB 11 00 00"),
-		std::make_pair(0x40AC1C, "75 05 E9 2F 12 00 00 48 8B 84 24 10 1F 00 00 0F"),
-		std::make_pair(0x5EFE2C, "75 05 E9 2F 12 00 00 48 8B 84 24 10 1F 00 00 0F"),
-		std::make_pair(0x59F89C, "75 05 E9 2F 12 00 00 48 8B 84 24 10 1F 00 00 0F"),
-		std::make_pair(0x40A5B7, "75 05 E9 94 18 00 00 48 8B 84 24 10 1F 00 00 0F"),
-		std::make_pair(0x5EF7C7, "75 05 E9 94 18 00 00 48 8B 84 24 10 1F 00 00 0F"),
-		std::make_pair(0x59F237, "75 05 E9 94 18 00 00 48 8B 84 24 10 1F 00 00 0F"),
-		std::make_pair(0x40114E, "75 05 E9 C8 62 00 00 48 8B 84 24 C0 42 00 00 0F"),
-		std::make_pair(0x595DCE, "75 05 E9 C8 62 00 00 48 8B 84 24 C0 42 00 00 0F"),
-		std::make_pair(0x5E635E, "75 05 E9 C8 62 00 00 48 8B 84 24 C0 42 00 00 0F"),
-		std::make_pair(0x496C4A, "0F 85 CB 00 00 00 48 8B 84 24 70 0A 00 00 0F B6"),
-		std::make_pair(0x4C9DBD, "C6 84 24 60 41 01 00 00 48 8D 84 24 80 28 01 00") // alert
-	};
+		std::istringstream iss(content);
+		std::string line;
 
-	else if (pVersion == "V3.8753")
-		addresses =
-	{
-		std::make_pair(0x4094C4, "75 05 E9 11 63 00 00 48 8B 84 24 20 43 00 00 0F"),
-		std::make_pair(0x40EB0F, "75 1A 8B 05 ED AF 56 00 FF C0 89 05 E5 AF 56 00"),
-		std::make_pair(0x4129DD, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x413226, "74 05 E9 2F 12 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x58C974, "75 05 E9 11 63 00 00 48 8B 84 24 20 43 00 00 0F"),
-		std::make_pair(0x591FBF, "75 1A 8B 05 3D 7B 3E 00 FF C0 89 05 35 7B 3E 00"),
-		std::make_pair(0x595E8D, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x5966D6, "74 05 E9 2F 12 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x608B84, "75 05 E9 11 63 00 00 48 8B 84 24 20 43 00 00 0F"),
-		std::make_pair(0x60E1CF, "75 1A 8B 05 2D B9 36 00 FF C0 89 05 25 B9 36 00"),
-		std::make_pair(0x61209D, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x6128E6, "74 05 E9 2F 12 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x659AF4, "75 05 E9 11 63 00 00 48 8B 84 24 20 43 00 00 0F"),
-		std::make_pair(0x65F13F, "75 1A 8B 05 BD A9 31 00 FF C0 89 05 B5 A9 31 00"),
-		std::make_pair(0x66300D, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x663856, "74 05 E9 2F 12 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x4D4645, "C6 84 24 E0 45 01 00 00 48 8D 84 24 20 2D 01 00") // alert
-	};
+		std::string curr_version;
 
-	else if (pVersion == "V3.87542")
-		addresses =
-	{
-		std::make_pair(0x6643D1, "75 1A 8B 05 2B B7 31 00 FF C0 89 05 23 B7 31 00"),
-		std::make_pair(0x66829D, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x65ED86, "75 05 E9 11 63 00 00 48 8B 84 24 80 43 00 00 0F"),
-		std::make_pair(0x613331, "75 1A 8B 05 CB C7 36 00 FF C0 89 05 C3 C7 36 00"),
-		std::make_pair(0x60DCE6, "75 05 E9 11 63 00 00 48 8B 84 24 80 43 00 00 0F"),
-		std::make_pair(0x6171FD, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x5967B1, "75 1A 8B 05 4B 93 3E 00 FF C0 89 05 43 93 3E 00"),
-		std::make_pair(0x591166, "75 05 E9 11 63 00 00 48 8B 84 24 80 43 00 00 0F"),
-		std::make_pair(0x59A67D, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x4100F1, "75 1A 8B 05 0B FA 56 00 FF C0 89 05 03 FA 56 00"),
-		std::make_pair(0x40AAA6, "75 05 E9 11 63 00 00 48 8B 84 24 80 43 00 00 0F"),
-		std::make_pair(0x413FBD, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x4D6C10, "C6 84 24 40 48 01 00 00 48 8D 84 24 00 2F 01 00") // alert
-	};
+		while (std::getline(iss, line)) {
+			if (line.find("AddVersionSupport|") != -1) {
+				RTParser parser = RTParser::parse(line);
+				auto data = parser.find("AddVersionSupport")->m_values;
+				PatchData p;
+				p.ProxyVersion = data[0];
+				p.sizeKB[0] = stoi(data[1]);
+				p.sizeKB[1] = stoi(data[2]);
+				patchData.push_back(p);
+			}
+			if (line.find("[START_") != -1) {
+				curr_version = line.substr(line.find("[START_") + 7);
+				if (curr_version.find("]") != -1) curr_version.erase(curr_version.begin() + curr_version.find("]"), curr_version.end());
 
-	else if (pVersion == "V3.87543")
-		addresses =
+				bool found = false;
+				for (PatchData p : patchData)
+					if (p.ProxyVersion == curr_version) {
+						found = true;
+						break;
+					}
+				if (!found) {
+					PatchData p;
+					p.ProxyVersion = curr_version;
+					patchData.push_back(p);
+				}
+			}
+			f_content += line + '\n';
+		}
+	}
 	{
-		std::make_pair(0x665601, "75 1A 8B 05 7B C5 31 00 FF C0 89 05 73 C5 31 00"),
-		std::make_pair(0x65FFB6, "75 05 E9 11 63 00 00 48 8B 84 24 80 43 00 00 0F"),
-		std::make_pair(0x597961, "75 1A 8B 05 1B A2 3E 00 FF C0 89 05 13 A2 3E 00"),
-		std::make_pair(0x6694CD, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x6143B1, "75 1A 8B 05 CB D7 36 00 FF C0 89 05 C3 D7 36 00"),
-		std::make_pair(0x60ED66, "75 05 E9 11 63 00 00 48 8B 84 24 80 43 00 00 0F"),
-		std::make_pair(0x61827D, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x592316, "75 05 E9 11 63 00 00 48 8B 84 24 80 43 00 00 0F"),
-		std::make_pair(0x59B82D, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		std::make_pair(0x4112C1, "75 1A 8B 05 BB 08 57 00 FF C0 89 05 B3 08 57 00"),
-		std::make_pair(0x40BC76, "75 05 E9 11 63 00 00 48 8B 84 24 80 43 00 00 0F"),
-		std::make_pair(0x41518D, "75 05 E9 78 1A 00 00 48 8B 84 24 40 20 00 00 0F"),
-		//std::make_pair(0x669D16, "75 05 E9 2F 12 00 00 48 8B 84 24 40 20 00 00 0F"),// PACKET_STATE
-		std::make_pair(0x4D7DE0, "C6 84 24 40 48 01 00 00 48 8D 84 24 00 2F 01 00") // alert
-	};
+		int i = 0;
+		while (i < patchData.size()) {
+			std::string version = patchData[i].ProxyVersion;
+			std::string data = f_content.substr(f_content.find("[START_" + version + "]\n") + ("[START_" + version + "]\n").length() - 1);
+			if (data.find("[END_" + version + "]") != -1) data.erase(data.begin() + data.find("[END_" + version + "]"), data.end());
 
-	else if (pVersion == "V3.87544")
-		addresses =
-	{
-		std::make_pair(0x4661F0, "75 05 E9 A5 63 00 00 48 8B 84 24 10 46 00 00 0F"),
-		std::make_pair(0x46B8C0, "75 1A 8B 05 BC 22 5C 00 FF C0 89 05 B4 22 5C 00"),
-		std::make_pair(0x470348, "75 05 E9 DB 1C 00 00 48 8B 84 24 70 24 00 00 0F"),
-		std::make_pair(0x609B60, "75 05 E9 A5 63 00 00 48 8B 84 24 10 46 00 00 0F"),
-		std::make_pair(0x60F230, "75 1A 8B 05 4C E9 41 00 FF C0 89 05 44 E9 41 00"),
-		std::make_pair(0x613CB8, "75 05 E9 DB 1C 00 00 48 8B 84 24 70 24 00 00 0F"),
-		std::make_pair(0x69BFB0, "75 05 E9 A5 63 00 00 48 8B 84 24 10 46 00 00 0F"),
-		std::make_pair(0x6A1680, "75 1A 8B 05 FC C4 38 00 FF C0 89 05 F4 C4 38 00"),
-		std::make_pair(0x6A6108, "75 05 E9 DB 1C 00 00 48 8B 84 24 70 24 00 00 0F"),
-		std::make_pair(0x6FDB20, "75 05 E9 A5 63 00 00 48 8B 84 24 10 46 00 00 0F"),
-		std::make_pair(0x7031F0, "75 1A 8B 05 8C A9 32 00 FF C0 89 05 84 A9 32 00"),
-		std::make_pair(0x707C78, "75 05 E9 DB 1C 00 00 48 8B 84 24 70 24 00 00 0F"),
-		std::make_pair(0x546DAE, "C6 84 24 60 48 01 00 00 48 8D 84 24 20 2F 01 00") // alert
-	};
+			std::string line;
+			std::istringstream iss(data);
+
+			while (std::getline(iss, line)) {
+				if (line.rfind("0x", 0) == 0) {
+					uintptr_t addr = stoull(line.substr(0, line.find("  ")), nullptr, 0);
+					std::string bytes = line.substr(line.find("  ") + 2);
+
+					patchData[i].addresses.push_back(
+						std::make_pair((int)addr, bytes)
+					);
+				}
+			}
+			i++;
+		}
+	}
+}
+
+bool FetchPatchData() {
+	printf("Fetching Data From Web...\r");
+
+	//printf("==========[CURL RESPONSE]==========\n");
+	system("curl https://raw.githubusercontent.com/JoakimTheCoder/SilviozasAutoCrack/refs/heads/main/PatchData -s -o PatchData.txt");
+	//printf("==========[END CURL RESPONSE]==========\n");
+
+	std::ifstream file("PatchData.txt");
+	if (!file.is_open())
+		return false;
+
+	std::string content, line;
+	while (std::getline(file, line))
+		content += line + '\n';
+	LoadPatchData(content);
+	return true;
+}
+
+PatchData Initialize(int sizeKB) {
+	for (PatchData p : patchData) {
+		if (p.sizeKB[0] == sizeKB ||
+			p.sizeKB[1] == sizeKB)
+			return p;
+	}
+	return {};
 }
 
 int main() {
-	/*   (If new version is out, tell joakimmer1k0)    */
 
 	printf("Silviozas Auto Crack Made By joakimmer1k0. Any problems or need help? Message me in Discord.\n\n");
 
+	if (!FetchPatchData()) {
+		printf("Couldn't fetch Patch Data for AutoCrack (check your internet and try again.)\n");
+		Sleep(-1);
+	}
+	printf("Currently Supported Proxy Versions: [");
+	for (PatchData p : patchData) printf("%s (%dkb) ", p.ProxyVersion.c_str(), p.sizeKB[1]);
+	printf("]\n");
+
+	//for (PatchData p : patchData) {
+	//	printf("START=======[VERSION: %s]=======\n", p.ProxyVersion.c_str());
+	//	for (auto a : p.addresses)
+	//		printf("[Address: %d]  [Bytes: %s]\n", a.first, a.second.c_str());
+	//	printf("END=======[VERSION: %s]=======\n", p.ProxyVersion.c_str());
+	//}
+
 	if ((int)new_ownerid.length() != 10 || (int)new_secret.length() != 64) {
 		printf("[ERROR]: new_ownerid and new_secret lengths has to be 10 and 64.\n");
-		while (true)
-			Sleep(1);
+		Sleep(-1);
 	}
 
 	std::string path = R"(C:\Users\Public\Proxy_Stuff\Silviozas Premium Proxy.exe)";
@@ -274,45 +274,24 @@ int main() {
 	//printf("KB: %d\n", sizeKB);
 	in.seekg(0, std::ios::beg);
 
-	switch (sizeKB) {
+	PatchData data = Initialize(sizeKB);
 
-	case 9640:
-		GetAdresses("V3.8753");
-		break;
-
-	case 10383:
-	case 10382:
-		GetAdresses("V3.8753");
-		break;
-
-	case 10405:
-	case 10406:
-		GetAdresses("V3.87542");
-		break;
-
-	case 10412:
-	case 10413:
-		GetAdresses("V3.87543");
-		break;
-
-	case 11078:
-	case 11079:
-		GetAdresses("V3.87544");
-		break;
-
-	default:
+	if (data.addresses.empty()) {
 		printf("This Proxy Version Isn't Supported. If There's A New Update, Tell Joakim.\n");
 		Sleep(-1);
+		return 0;
 	}
 
-	printf("Current Proxy Version Is: %s\n\n", current_version.c_str());
+	printf("Current Proxy Version Is: %s\n\n", data.ProxyVersion.c_str());
+	
+	current_version = data.ProxyVersion;
 
-	printf("Starting patch...\n\n");
-	edit_string(path, original_ownerid, new_ownerid, std::stof(current_version.substr(1)) >= 3.8753f ? 40 : -1);
+	edit_string(path, original_ownerid, new_ownerid, stof(data.ProxyVersion.substr(1)) >= 3.8753f ? 40 : -1);
 
 	edit_string(path, original_secret, new_secret);
 
-	for (auto addr : addresses)
+	printf("\nStarting patch...\n\n");
+	for (auto addr : data.addresses)
 		patch(path, addr.first, addr.second);
 
 	printf("\nFinished Patching! (Proxy is ready to use)\n");
@@ -320,6 +299,3 @@ int main() {
 	Sleep(-1);
 	return 0;
 }
-
-
-
